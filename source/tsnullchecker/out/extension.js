e7dce28b937a28695cd77aa44138a8e9600e426d
hunter <nechanicky@programm.cz>
2019-02-19 19:53:14 +0100

    uvodni command pro check na null

---
+++ source/tsnullchecker/out/extension.js
@@ -0,0 +1,56 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+const vscode = require("vscode");
+// this method is called when your extension is activated
+// your extension is activated the very first time the command is executed
+function activate(context) {
+    // Use the console to output diagnostic information (console.log) and errors (console.error)
+    // This line of code will only be executed once when your extension is activated
+    console.log('Congratulations, your extension "tsnullchecker" is now active!');
+    // The command has been defined in the package.json file
+    // Now provide the implementation of the command with registerCommand
+    // The commandId parameter must match the command field in package.json
+    let disposable = vscode.commands.registerCommand('TsNullChecker.checkNulls', () => {
+        // The code you place here will be executed every time your command is executed
+        // Display a message box to the user
+        if (vscode.window.activeTextEditor && vscode.window.activeTextEditor.selection) {
+            const selection = vscode.window.activeTextEditor.selection;
+            const text = vscode.window.activeTextEditor.document.getText(selection);
+            const resultParts = [];
+            let actualPart = '';
+            for (const part of text.split('.')) {
+                if (part === 'this') {
+                    actualPart = part;
+                    continue;
+                }
+                const indexRegex = /\[[.+]+\]/;
+                let match = indexRegex.exec(part);
+                while (match) {
+                    resultParts.push(combineParts(actualPart, part.substr(0, match.index)));
+                    match = indexRegex.exec(part);
+                }
+                actualPart = combineParts(actualPart, part);
+                resultParts.push(actualPart);
+            }
+            console.debug(resultParts);
+            const finalText = 'if(' + resultParts.join('\n&& ') + '){\n' + text + '\n}';
+            vscode.window.activeTextEditor.edit(editBuilder => editBuilder.replace(selection, finalText));
+        }
+    });
+    context.subscriptions.push(disposable);
+}
+exports.activate = activate;
+function combineParts(actualPart, newPart) {
+    if (actualPart && actualPart !== '') {
+        return actualPart + '.' + newPart;
+    }
+    else {
+        return newPart;
+    }
+}
+// this method is called when your extension is deactivated
+function deactivate() { }
+exports.deactivate = deactivate;
+//# sourceMappingURL=extension.js.map
